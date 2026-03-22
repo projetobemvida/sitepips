cbbb38e1c53d53392ce571b0e231418185f4c2ea
projetobemvida <157609531+projetobemvida@users.noreply.github.com>
2026-03-22 16:33:08 -0300

    Update cookies-lgpd.js

--- js/cookies-lgpd.js
+++ js/cookies-lgpd.js
@@ -487,10 +487,21 @@ class CookieConsentManager {
         console.log('Google Analytics via GTM autorizado');
     }
 
-    loadFacebookPixel() {
-        // Deixamos o Google Tag Manager (GTM) cuidar do carregamento
-        console.log('Facebook Pixel via GTM autorizado');
+   loadFacebookPixel() {
+    if (typeof fbq === 'undefined') {
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window,document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
     }
+    fbq('init', '9861765200617530');
+    fbq('track', 'PageView');
+    console.log('Facebook Pixel via GTM autorizado');
+}
 
     loadFunctionalCookies() {
         // Implementar cookies funcionais conforme necessário
